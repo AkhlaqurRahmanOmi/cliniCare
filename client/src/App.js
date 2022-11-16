bd83c0031c83679cd0b858df141a383b483004a6
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmail.com>
2022-11-16 17:18:17 +0600

    adding frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
-const App = () => {
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/home.js';
+import {browserRouter as Router, Switch, Route } from 'react-router-dom'
+function App() {
     return (
         <div>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                </Switch>
+            </Router>
             <h1>CliniCare</h1>
         </div>
-    )
+    );
 }
 
 export default App;
